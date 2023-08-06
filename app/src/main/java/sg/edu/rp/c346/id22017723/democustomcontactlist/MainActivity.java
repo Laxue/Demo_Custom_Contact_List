59ff2cb433a05dab8ac96eed884c53ebb742ca05
Jane Tung <22017723@myrp.edu.sg>
2023-08-06 20:42:31 +0800

    FINISHED

--- app/src/main/java/sg/edu/rp/c346/id22017723/democustomcontactlist/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22017723/democustomcontactlist/MainActivity.java
@@ -3,6 +3,15 @@ package sg.edu.rp.c346.id22017723.democustomcontactlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView iv;
+        ListView lvContact;
+        ArrayList<Contact> alContactList;
+        CustomAdapter caContact;
+
+//        iv = findViewById(R.id.iv);
+        lvContact = findViewById(R.id.listViewContacts);
+        alContactList = new ArrayList<>();
+        Contact item1 = new Contact("Mary", 65, 1234567, 'F');
+        Contact item2 = new Contact("Ken", 65, 7654321, 'M');
+        alContactList.add(item1);
+        alContactList.add(item2);
+
+//        String imageUrl = "https://i.imgur.com/tGbaZCY.jpg";
+//        Picasso.with(this).load(imageUrl).into(iv);
+        caContact = new CustomAdapter(this, R.layout.row, alContactList);
+        lvContact.setAdapter(caContact);
     }
+
 }
